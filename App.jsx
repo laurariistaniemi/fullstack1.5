7fe5827c65f698cbc8ac9ceca1cbfa6d99e6a86d
laurariistaniemi <162997772+laurariistaniemi@users.noreply.github.com>
2024-06-26 18:41:43 +0300

    1.5
    tehtävä 1.5 joka on rakennettu aiempien päälle

--- App.jsx
+++ App.jsx
@@ -13,7 +13,7 @@ const Part = (props) => {
   console.log(props)
   return (
     <p>
-      {props.part.name} {props.part.exercises}
+      {props.parts}
     </p>
   )
 }
@@ -22,9 +22,9 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1}  />
-      <Part part={props.part2}  />
-      <Part part={props.part3}  />
+      
+      <Part part={props.parts}/>
+      
     </div>
   )
 }
@@ -32,9 +32,12 @@ const Content = (props) => {
 //totalin alustus
 const Total = (props) => {
   console.log(props)
+  
+  
   return (
     <div> 
     Number of exercises: {props.total}
+    
     </div>
   )
 }
@@ -42,36 +45,44 @@ const Total = (props) => {
 
   //datat
   const App = () => {
-    const course = 'Half Stack application development'
-    const part1 = {
-      name: 'Fundamentals of React',
-      exercises: 10
+    const course = {
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10 
+        },
+        {
+          name:'Using props to pass data',
+          exercises: 7
+        },
+        {
+          name: 'State of a component',
+          exercises: 14
+        }
+      ]
+      
     }
-    const part2 = {
-      name: 'Using props to pass data',
-      exercises: 7
-    }
-    const part3 = {
-      name: 'State of a component',
-      exercises: 14
-    }
+    
+    //console.log(course["parts"][0]["exercises"])
   
 
   //tulostus
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        //exercises1={exercises1}
-        part2={part2}
-        //exercises2={exercises2}
-        part3={part3}
-        //exercises3={exercises3}
-      />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+        {/*kurssinimi*/}
+      <Header course={course.name} /> 
+
+      {/*kurssien nimet, eriteltynä kätevyyden takia suoraan*/}
+      <Part parts={course.parts[0].name + " " + course.parts[0].exercises} />
+      <Part parts={course.parts[1].name + " " + course.parts[1].exercises}/> 
+      <Part parts={course.parts[2].name + " " +  course.parts[2].exercises}/>
+
+      {/*kurssien määrä*/}
+      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}/>
+      
     </div>
-  )
+    )
   }
 
 export default App
